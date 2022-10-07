4916c3814d75bf8499c68879796bb4ba6398d07f
unknown <vitor_gha@hotmail.com>
2022-10-07 11:30:54 -0300

    Outras aulas

---
+++ conversoes.js
@@ -0,0 +1,19 @@
+// tipo de dados
+//booleanos
+
+//conversão implícita
+const numero = 456;
+const numeroString = "456a";
+
+console.log(numero + numeroString)
+
+
+
+
+//conversão explícita
+//Number()
+//String()
+
+
+
+console.log(numero + Number(numeroString))
